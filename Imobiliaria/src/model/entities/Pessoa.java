0e9ad5e7cb4385a7b27e568ec7d123e82732984e
Alexandre Castro <alexandre.acrsf@gmail.com>
2019-12-04 00:57:58 -0300

    Adicionando getters and setter na classe abstrata

--- Imobiliaria/src/model/entities/Pessoa.java
+++ Imobiliaria/src/model/entities/Pessoa.java
@@ -11,5 +11,29 @@ public abstract class Pessoa  {
 		this.telefone = telefone;
 	}
 	
+	public String getNome() {
+		  return nome;
+		 }
+
+		 public void setNome(String nome) {
+		  this.nome = nome;
+		 }
+
+		 public int getTelefone() {
+		  return telefone;
+		 }
+
+		 public void setTelefone(int telefone) {
+		  this.telefone = telefone;
+		 }
+
+		 public int getCpf() {
+		  return cpf;
+		 }
+
+		 public void setCpf(int cpf) {
+		  this.cpf = cpf;
+		 }
+	
 	
 }
