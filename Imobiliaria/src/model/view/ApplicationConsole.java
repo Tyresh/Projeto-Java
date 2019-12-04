1b086052781ddc937d956f65920c73d9e7d3f0a2
Tyresh <leohenrique2207@hotmail.com>
2019-12-04 02:03:57 -0300

    Added instance for imovelList and added imovel1 to imovel5 to it

--- Imobiliaria/src/model/view/ApplicationConsole.java
+++ Imobiliaria/src/model/view/ApplicationConsole.java
@@ -7,9 +7,12 @@ import model.enums.*;
 public class ApplicationConsole {
 	public static void main (String[] args) {
 		
+		ImovelList imovelList = new ImovelList();
+		
 		Imobiliaria imob1 = new Imobiliaria("12345678901113", "Rua saudades dela", "Lego");
 		Imobiliaria imob2 = new Imobiliaria("21345678901113", "Rua foda que ela é linda",
-				"Playmobil");	
+				"Playmobil");
+		
 		Imovel imovel1 = new Imovel(122134, "CASA","DISPONIVEL", "Rua 3",
 				"Castelo do Drácula", 300000,imob1);
 		Imovel imovel2 = new Imovel(451241, "APARTAMENTO", "DISPONIVEL", "Rua 2",
@@ -20,6 +23,12 @@ public class ApplicationConsole {
 				"Doce Lar", 300000, imob2);
 		Imovel imovel5 = new Imovel(553857, "APARTAMENTO", "DISPONIVEL", "Rua 5",
 				"Oasis", 400000, imob1);
+		
+		imovelList.addImovel(imovel1);
+		imovelList.addImovel(imovel2);
+		imovelList.addImovel(imovel3);
+		imovelList.addImovel(imovel4);
+		imovelList.addImovel(imovel5);
 
 	}
 }
