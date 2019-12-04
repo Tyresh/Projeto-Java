641e1e76d95330c16f846378c975841b72a17461
Tyresh <leohenrique2207@hotmail.com>
2019-12-04 01:07:10 -0300

    Fuck this

--- Imobiliaria/src/model/view/ApplicationConsole.java
+++ Imobiliaria/src/model/view/ApplicationConsole.java
@@ -6,13 +6,6 @@ import model.enums.*;
 
 public class ApplicationConsole {
 	public static void main (String[] args) {
-
-		Cliente leo = new Cliente();
-		System.out.println(leo);
-		Contrato cont = new Contrato();
-		System.out.println("---------------------------");
-		System.out.println(cont);
-
 		
 	}
 }
